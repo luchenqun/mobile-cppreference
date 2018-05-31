c09fede62ccb6f37ae784ed9aab3d3fcaa3079cb
luchenqun <luchenqun@juzix.io>
2018-05-31 18:07:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mobile-cppreference
+
+> 使用手机阅读 http://zh.cppreference.com 很不方便，自己在撸一个，方便自己随时随地查看。
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3001
+npm run dev
+
---
+++ app.js
@@ -0,0 +1,38 @@
+const Koa = require("koa");
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const os = require("os");
+
+const app = new Koa();
+
+app.use(async ctx => {
+  let path = ctx.path.substr(1);
+  let url = "http://zh.cppreference.com/w/%E9%A6%96%E9%A1%B5";
+  console.log("path =[" + path + "]");
+  if (/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/.test(path)) {
+    url = path;
+  } else {
+    url = "http://zh.cppreference.com/" + path;
+  }
+
+  var options = {
+    uri: url,
+    transform: body => cheerio.load(body)
+  };
+
+  try {
+    let $ = await rp(options);
+    $("a").each(function(i) {
+      // windows平台开发，Linux 平台部署
+      this.href = (os.platform() == "linux" ? "http://cpp.luchenqun.com/" : "http://127.0.0.1:3001/") + this.href;
+    });
+    ctx.body = $.html();
+  } catch (error) {
+    ctx.body = JSON.stringify(error);
+  }
+
+  if (path == "/") {
+  }
+});
+
+app.listen(3001);
