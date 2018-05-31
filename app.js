a3ea3fbb69a5ea7823fd7fae40290196c4651a84
luchenqun <luchenqun@qq.com>
2018-05-31 23:33:05 +0800

    引入自己的style与init

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const Koa = require("koa");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const os = require("os");
+const path = require("path");
 
 const app = new Koa();
+
+app.use(require("koa-static")("."));
+app.use(require("@koa/cors")());
 
 app.use(async ctx => {
   let path = ctx.path.substr(1);
@@ -22,17 +26,23 @@ app.use(async ctx => {
 
   try {
     let $ = await rp(options);
-    $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">');
+    $("head").prepend('<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>');
+    $("head").prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">');
+    $("head").append('<link href="/style.css" rel="stylesheet">');
+    $("head").append('<script src="/init.js"></script>');
+
     $("a").each(function(i) {
       // windows平台开发，Linux 平台部署
       this.href = (os.platform() == "linux" ? "http://cpp.luchenqun.com/" : "http://127.0.0.1:3001/") + this.href;
     });
+    $("img").each(function(i) {
+      if (!/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/.test(this.src)) {
+        this.src = "http://zh.cppreference.com" + this.src;
+      }
+    });
     ctx.body = $.html();
   } catch (error) {
     ctx.body = JSON.stringify(error);
-  }
-
-  if (path == "/") {
   }
 });
 
---
+++ init.js
@@ -0,0 +1,6 @@
+$(function() {
+  $("body").css("min-width", "90%");
+  $("#content").css("max-width", "100%");
+  $("table").css("max-width", "100%");
+  $(".t-dcl-begin tbody").css("max-width", "100%");
+});
