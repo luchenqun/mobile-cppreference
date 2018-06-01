461254b458b8b21c8c0810e4c0dc7f449d442656
luchenqun <luchenqun@juzix.io>
2018-06-01 15:53:30 +0800

    溢出的table使用scroll解决

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.use(require("@koa/cors")());
 app.use(async ctx => {
   let path = ctx.path.substr(1);
   let url = "http://zh.cppreference.com/w/%E9%A6%96%E9%A1%B5";
+  
+  var deviceAgent = ctx.req.headers["user-agent"].toLowerCase();
+  var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
   console.log("path =[" + path + "]");
+
   if (/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/.test(path)) {
     url = path;
   } else {
@@ -28,19 +32,32 @@ app.use(async ctx => {
     let $ = await rp(options);
     $("head").prepend('<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>');
     $("head").prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">');
-    $("head").append('<link href="/style.css" rel="stylesheet">');
     $("head").append('<script src="/init.js"></script>');
 
     $("a").each(function(i) {
       // windows平台开发，Linux 平台部署
       this.href = (os.platform() == "linux" ? "http://cpp.luchenqun.com/" : "http://127.0.0.1:3001/") + this.href;
     });
+
     $("img").each(function(i) {
-      console.log($(this).attr('src'));
       if (!/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/.test(this.src)) {
-        this.src = "http://zh.cppreference.com" + this.src;
+        $(this).attr("src", "http://zh.cppreference.com" + $(this).attr("src"));
       }
     });
+
+    // 特殊处理容器库的表格
+    if (agentID && path == "w/cpp/container") {
+      $("#mw-content-text")
+        .children()
+        .last()
+        .css("overflow", "scroll");
+
+      $("#mw-content-text")
+        .children()
+        .last()
+        .css("display", "");
+    }
+
     ctx.body = $.html();
   } catch (error) {
     ctx.body = JSON.stringify(error);
--- init.js
+++ init.js
@@ -1,8 +1,40 @@
 $(function() {
+
+  var u = navigator.userAgent;
+  var device = { //移动终端浏览器版本信息
+      trident: u.indexOf('Trident') > -1, //IE内核
+      presto: u.indexOf('Presto') > -1, //opera内核
+      webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+      gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+      mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+      ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+      android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+      iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+      iPad: u.indexOf('iPad') > -1, //是否iPad
+      webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+  };
+
+
+  if (!device.mobile)  return;
+
   $("body").css("min-width", "90%");
   $("#content").css("max-width", "100%");
-  $("table").css("max-width", "100%");
-  $(".t-dcl-begin tbody").css("max-width", "100%");
+
+  $("table").css("table-layout", "fixed");
+  $("table").wrap("<div style='width:100%;overflow:scroll;'></div>");
+
+  // 删除 "运行此代码"
+  $(".t-example-live-link").remove();
+  $(".mw-geshi").css("width", "100%");
+  $("pre").css("width", "100%");
+  $(".mw-geshi").css("padding", "0px");
+  $("pre").css("padding", "10px");
+
+  // 头部
   $("#cpp-head-first").css("max-width", "100%");
   $("#cpp-head-second").css("max-width", "100%");
+
+  // 尾部
+  $("#footer").css("max-width", "100%");
+  $("#cpp-footer-base").css("max-width", "100%");
 });
